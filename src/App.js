ab752748e5be2ef8a299bf539928e6fd074654fd
rudy.hneini <rudy.hneini@cmeoffshore.com>
2023-04-03 18:40:02 +0300

    Back to Original Page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 import SearchIcon from './search.svg';
-import heartIcon from './heartsearch.svg';
 import MovieCard from './MovieCard'
 
 const API_URL = "http://www.omdbapi.com?apikey=9118604";
@@ -9,7 +8,6 @@ const API_URL = "http://www.omdbapi.com?apikey=9118604";
 const App = () => {
     const [movies, setMovies] = useState([]);
     const [searchTerm, setSearchTerm] = useState('');
-    const [searchWeb, setSearchWeb] = useState('');
 
     const searchMovies = async (title) => {
         const response = await fetch(`${API_URL}&s=${title}`);
@@ -24,68 +22,42 @@ const App = () => {
 
     return (
         <div className='app'>
-  
-            <h4 style={{ color: 'red' }}>ENJOY</h4>
-            <p style={{ color: 'blue' }}>Fun</p>
+            
+            <h1>MovieLand</h1>
+            
+            <div className='search'>
+                <input
+                  placeholder='Search for movies'
+                  value={searchTerm}
+                  onChange={(e) => setSearchTerm(e.target.value)} 
+                />
+                {/* <img
+                  id="searchicon"
+                  src={SearchIcon}
+                  alt="search"
+                  onClick={() => searchMovies(searchTerm)}
+                /> */}
+                <button id='searchicon' onClick={() => searchMovies(searchTerm)}>
+                    <img src={SearchIcon} alt="search" />
+                </button>
+
+            </div>
 
             {
                 movies?.length > 0
                   ? (
-                    <div>
+                    <div className='container'>
                       {movies.map((movie) => (
                           <MovieCard movie={movie} />
                       ))}
                     </div> 
                   ) : (
-                    <div>
+                    <div className='empty'>
                       <h2>No movies found</h2>
                     </div>
                   )
-            }
-
-                        
-                <h2 style={{ color: 'red' }}>Go ahead search</h2>
-
-                <input
-                  value={searchTerm}
-                  onChange={(e) => setSearchTerm(e.target.value)} 
-                />
-                
-                <h4 style={{ color: 'blue' }}>Take a look</h4>
-
-                <h2 style={{ color: 'white' }}>Movies</h2>
-
-                <p style={{ color: 'red' }}>Wait and see</p>
-
-                <img
-                  src={heartIcon}
-                  id="searchicon"
-                  onClick={() => searchMovies(searchTerm)}
-                />
-
-                <h3 style={{ color: 'green' }}>Open a new website</h3>
-
-                <div>
-                  <input
-                    value={searchWeb}
-                    placeholder='Navigate to'
-                    onChange={(e) => setSearchWeb(e.target.value)} 
-                  />
-                  <img
-                    src={SearchIcon}
-                    id="navigating"
-                    onClick={() => window.open(searchWeb, '_blank')}
-                  />
-                </div>
-
-                <h4 style={{ color: 'green' }}>Finally</h4>
-
-
-
-                   
+            }        
         </div>
-
-        
 
     );
 }
